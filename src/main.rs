163933d7d0871413e290430ab5a8862d8a34418a
Maxim Kosterov <maxim.kosterov@gmail.com>
2021-07-28 23:53:18 +0300

    demo functionality

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::{
     collections::{HashMap, HashSet},
-    fs::read_to_string,
+    fs::{read, read_to_string, write},
     path::PathBuf,
 };
 
 use anyhow::Context;
+use data_encoding::BASE32HEX_NOPAD;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -16,8 +17,8 @@ async fn main() -> anyhow::Result<()> {
     let data_path = angopath.join("data");
 
     // getting ango.yml configs
-    let (hashmap, hashset) = {
-        let filecontents = read_to_string(angoyml_path).context("Was not able to read ango.yml")?;
+    let (mut hashmap, hashset) = {
+        let filecontents = read_to_string(&angoyml_path).context("failed to read ango.yml")?;
         if filecontents != "" {
             let hashmap: HashMap<String, String> =
                 serde_yaml::from_str(&filecontents).context("failed to parse ango.yml")?;
@@ -28,7 +29,26 @@ async fn main() -> anyhow::Result<()> {
         }
     };
 
-    dbg!(data_path, hashmap, hashset);
+    // adding .gitignore to the filelist
+    {
+        let entryname = ".gitignore".to_string();
+        let contents = read("./.gitignore").context("failed to open ./.gitignore")?;
+        let hash = BASE32HEX_NOPAD.encode(blake3::hash(&contents).as_bytes());
+
+        if !hashset.contains(&hash) {
+            hashmap.insert(entryname.clone(), hash.clone());
+
+            // writing file
+            write(data_path.join(hash), contents)
+                .with_context(|| format!("Failed to write {}", entryname))?;
+        } else if !hashmap.contains_key(&entryname) {
+            hashmap.insert(entryname.clone(), hash);
+        }
+
+        // writing ango.yml
+        let hashmap = serde_yaml::to_string(&hashmap).context("failed to encode ango.yml")?;
+        write(&angoyml_path, hashmap).context("failed to save ango.yml")?;
+    }
 
     Ok(())
 }
