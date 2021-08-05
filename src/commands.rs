c8636fdc35aff2a8a07abce2bf675952183b7132
Maxim Kosterov <maxim.kosterov@gmail.com>
2021-08-06 01:41:23 +0300

    added add_object function

--- src/commands.rs
+++ src/commands.rs
@@ -18,29 +18,14 @@ pub fn add(
     if meta.is_file() {
         // getting file
         let contents = read(path).context("failed to open FILE")?;
-        let hash = BASE32HEX_NOPAD.encode(blake3::hash(&contents).as_bytes());
 
-        // checking for existence
-        if !context.objects.contains(&hash) {
+        let (hash, added) = add_object(&contents, context, data_path)?;
+        if added || !context.links.contains_key(&epname) {
             context.links.insert(
                 epname.clone(),
                 TypedHash {
                     ty: LinkType::File,
                     hash: hash.clone(),
-                },
-            );
-            context.objects.insert(hash.clone());
-
-            // writing file
-            write(data_path.join(hash), contents)
-                .with_context(|| format!("failed to write {}", epname))?;
-        } else if !context.links.contains_key(&epname) {
-            // adding endpoint, since the file exists
-            context.links.insert(
-                epname.clone(),
-                TypedHash {
-                    ty: LinkType::File,
-                    hash,
                 },
             );
         } else {
@@ -54,5 +39,26 @@ pub fn add(
         Err(anyhow::anyhow!("failed to open FILE as file"))
     } else {
         Err(anyhow::anyhow!("failed to open FILE as directory or file"))
+    }
+}
+
+// returns true if the hash was not in the set
+fn add_object(
+    contents: &[u8],
+    context: &mut AngoContext,
+    data_path: &PathBuf,
+) -> anyhow::Result<(String, bool)> {
+    let hash = BASE32HEX_NOPAD.encode(blake3::hash(&contents).as_bytes());
+
+    // checking for existence
+    if !context.objects.contains(&hash) {
+        context.objects.insert(hash.clone());
+
+        // writing the file
+        write(data_path.join(&hash), contents)
+            .with_context(|| format!("failed to write {}", hash))?;
+        Ok((hash, true))
+    } else {
+        Ok((hash, false))
     }
 }
